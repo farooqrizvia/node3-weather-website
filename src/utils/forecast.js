482501aa5ad4f0df5ad772284e714fb4856fe02a
farooqrizvia <51982315+farooqrizvia@users.noreply.github.com>
2019-11-15 19:46:44 +0530

    added dev dependencies

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary +`It is currently ${body.currently.temperature} degree out. \n The high today is ${body.daily.data[0].temperatureHigh} and temperature low is ${body.daily.data[0].temperatureLow}. \nThere is a ${body.currently.precipProbability}% chance of rain.`)
+            callback(undefined, body.daily.data[0].summary +`It is currently ${body.currently.temperature} degree out.  The high today is ${body.daily.data[0].temperatureHigh} and temperature low is ${body.daily.data[0].temperatureLow}. \nThere is a ${body.currently.precipProbability}% chance of rain.`)
         }
     })
 
